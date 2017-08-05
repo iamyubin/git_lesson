a7c86e24292d8d0ac3bcb71340be2d9d1d88ebf4
yubin <yubin2007@hotmail.com>
2017-08-05 17:42:12 +0800

    added a second function for calculating square

--- src/square.py
+++ src/square.py
@@ -1,7 +1,12 @@
-def square(z):
+def square(y):
     """
     Takes a value and returns the squared value,
     Uses the ** operator.
     """
-    return z * z
+    return y ** 2
+
+def square2(x):
+    return x * x
+
 print(square(10))
+print(square2(10))
